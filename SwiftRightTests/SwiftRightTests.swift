d819b6dbf799b5f3e758a9b012190de9e091824a
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2020-04-17 17:38:47 -0500

    Initial Commit

---
+++ SwiftRightTests/SwiftRightTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftRightTests.swift
+//  SwiftRightTests
+//
+//  Created by Nathan Lawrence on 4/17/20.
+//  Copyright © 2020 Nathan Lawrence. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftRight
+
+class SwiftRightTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
